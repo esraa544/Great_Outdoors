fbff6aed6b1900a8d9b3b9a38efb043fc922ecb2
Esraa Mohamed <76407362+esraa544@users.noreply.github.com>
2022-09-24 23:49:02 +0200

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+//Responsive Navbar
+let menuList = document.getElementById("menuList");
+let menuIcon = document.getElementById("menu_icon");
+
+menuIcon.addEventListener("click", function togglemenu(){
+    menuList.classList.toggle("res_heigth");
+        } );
